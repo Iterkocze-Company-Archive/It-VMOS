34143580823b80615f2d93996a6a900febcaca56
5271 <piotr--tomaszewski@wp.pl>
2020-12-20 19:03:36 +0100

    system information update
    i forgot to change the information about system version :|

--- ItDosSystemPlikow/InformacjeSystem.cs
+++ ItDosSystemPlikow/InformacjeSystem.cs
@@ -7,7 +7,7 @@ namespace CosmosKernel2
         public static void Informacje_o_Systemie()
         {
             Console.WriteLine("System: Iterkocze Company. (R) IT-DOS 32-Bit OS");
-            Console.WriteLine("Wersja systemu: 0.0.5.6FS: Service Pack 1");
+            Console.WriteLine("Wersja systemu: 0.0.5.7: Service Pack 1");
             Console.WriteLine("Data wydania: 17.12.2020");
             Console.WriteLine("RAM: " + Cosmos.Core.CPU.GetAmountOfRAM() + " MB");
             Console.WriteLine($"Ilosc wolnego miejsca: {Kernel.fs.GetAvailableFreeSpace("0:/") / 1000000}MB");
